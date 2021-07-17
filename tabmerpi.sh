144e94fc0954a628c701cd40db792fd2dcede598
ombahiwal@gmail.com <ombahiwal@gmail.com>
2021-07-17 23:47:58 +0530

    setup script2

--- tabmerpi.sh
+++ tabmerpi.sh
@@ -1,6 +1,15 @@
 sudo apt-get -y update
 sudo apt-get install -y git python3 pip3
+sudo apt-get install paps
+pip3 install pyowm tweepy requests
+pip3 install textwrap argparse tabulate json requests
+mkdir tabme-print
+cd tabme-print
+git init
+git remote add origin https://github.com/ombahiwal/tabme-printer-py
+git pull origin master
 git clone https://github.com/OkkarMin/HOP-H58-RaspberryPi-Driver
+cd ../
 cd HOP-H58-RaspberryPi-Driver
 chmod +x ./install_H58_driver.sh
 ./install_H58_driver.sh
@@ -10,12 +19,4 @@ cd Raspberry-Pi-Thermal-Printer
 sudo chmod +x ./installer.sh
 sudo ./installer.sh
 cd ../
-sudo apt-get install paps
-pip3 install pyowm tweepy requests
-mkdir tabme-print
-cd tabme-print
-git init
-git remote add origin https://github.com/ombahiwal/tabme-printer-py
-git pull origin master
-pip3 install textwrap argparse tabulate json requests
 python3 tabme-queue-print.py

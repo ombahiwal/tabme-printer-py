181d69220627893c5f0b7d2fe289debce8507f39
ombahiwal@gmail.com <ombahiwal@gmail.com>
2021-07-18 00:33:49 +0530

    Install Script 3

---
+++ launcher.sh
@@ -0,0 +1,2 @@
+python3 ./tabme-auto-print.py &
+python3 ./tabme-queue-print.py &
--- tabmerpi.sh
+++ tabmerpi.sh
@@ -1,22 +1,22 @@
 sudo apt-get -y update
-sudo apt-get install -y git python3 pip3
-sudo apt-get install paps
-pip3 install pyowm tweepy requests
-pip3 install textwrap argparse tabulate json requests
+sudo apt-get install -y git python3 vim
+# sudo apt-get install -y cups ia32-libs gcc-multilib
+pip3 install argparse tabulate json requests textwrap3 
 mkdir tabme-print
 cd tabme-print
 git init
 git remote add origin https://github.com/ombahiwal/tabme-printer-py
 git pull origin master
-cd ../
-git clone https://github.com/OkkarMin/HOP-H58-RaspberryPi-Driver
-cd HOP-H58-RaspberryPi-Driver
-chmod +x ./install_H58_driver.sh
-./install_H58_driver.sh
+# Run on Startup
+echo 'python3 /root/tabme-print/tabme-auto-print.py &' >> /etc/rc.local
+echo 'python3 /root/tabme-print/tabme-queue-print.py &' >> /etc/rc.local
+# cd ../
+# git clone https://github.com/OkkarMin/HOP-H58-RaspberryPi-Driver
+# cd HOP-H58-RaspberryPi-Driver
+# chmod +x ./install_H58_driver.sh
+# ./install_H58_driver.sh
 cd ../
 git clone https://github.com/LukePrior/Raspberry-Pi-Thermal-Printer
 cd Raspberry-Pi-Thermal-Printer
 sudo chmod +x ./installer.sh
 sudo ./installer.sh
-cd ../
-python3 tabme-queue-print.py

638a48478a5ad9329a578b99b7d10cf15541262c
Omkar Bahiwal <ombahiwal@gmail.com>
2021-09-16 00:47:35 +0530

    Update tabme-auto-print.py
    Os.system paps uncomment and length print

--- tabme-auto-print.py
+++ tabme-auto-print.py
@@ -160,10 +160,11 @@ def check_orders():
         print_text = get_print_text(data)
     if len(print_text) > 10:
         printer.write(str(print_text) + "\n\n")
-    else:
-        printer.write(str(""))
+        os.system('paps --left-margin=14 --font=\"Monospace\" --cpi 17 print.txt | lp')
+#     else:
+#         printer.write(str(""))
     printer.close()
-    # os.system('paps --left-margin=14 --font=\"Monospace\" --cpi 17 print.txt | lp')
+   
 
 if __name__ == '__main__':
     while True:
